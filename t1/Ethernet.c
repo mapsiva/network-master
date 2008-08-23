8fda3ad018d4bba8ad4ef5c464bc25e356d82962
mapsiva <mapsiva@a04a7cc0-a954-0410-a178-6fe4ac297742>
2008-08-23 17:28:26 +0000

    modificacoes no ip

--- t1/Ethernet.c
+++ t1/Ethernet.c
@@ -3,6 +3,7 @@
 #include "Ethernet.h"
 #include "PackageHeader.h"
 #include <stdio.h>
+#include <netinet/in.h>
 
 CHAR_T*
 trace_ethernet(ETHERNET_HEADER* eth, int ID,  FRAME_HEADER *frh)
@@ -11,10 +12,21 @@ trace_ethernet(ETHERNET_HEADER* eth, int ID,  FRAME_HEADER *frh)
 	printf ("ETHER:\n");
 	printf ("ETHER: Packet %d\n", ID);
 	printf ("ETHER: Packet size = %u\n", (unsigned int) frh->capt_data);
-	printf ("ETHER: Destination  = %.02X:%02X:%02X:%02X:%02X:%.02X\n", eth->sender[0], eth->sender[1], eth->sender[2], eth->sender[3], eth->sender[4], eth->sender[5]);	
-	printf ("ETHER: Source       = %02X:%02X:%02X:%02X:%02X:%02X\n", eth->receiver[0], eth->receiver[1], eth->receiver[2], eth->receiver[3], eth->receiver[4], eth->receiver[5]);
+	
+		
+	
+	printf ("ETHER: Destination \t= %02X:%02X:%02X:%02X:%02X:%02X %s\n", eth->receiver[0], eth->receiver[1], eth->receiver[2], eth->receiver[3], eth->receiver[4], eth->receiver[5], (is_broadcast(eth->receiver)?"(brodcast)":""));
+	
+	printf ("ETHER: Source \t\t= %.02X:%02X:%02X:%02X:%02X:%.02X\n", eth->sender[0], eth->sender[1], eth->sender[2], eth->sender[3], eth->sender[4], eth->sender[5]);	
 	printf ("ETHER: Ethertype %04X\n",(unsigned int) ntohs(eth->type));
 	printf ("ETHER:\n\n");
 	return 0;
+}
+
+
+int 
+is_broadcast(BYTE * byte)
+{
+    return (*byte == 0xFF) && (*(byte+1) == 0xFF) && (*(byte+2) == 0xFF) && (*(byte+3) == 0xFF) && (*(byte+4) == 0xFF) && (*(byte+5) == 0xFF) ;
 }
 #endif 
