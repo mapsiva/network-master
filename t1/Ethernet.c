e75db4ec8d3a9b42d334bc9d05ba293d2c46a092
maxs.santos <maxs.santos@a04a7cc0-a954-0410-a178-6fe4ac297742>
2008-08-25 13:53:35 +0000

    Correcoes de conflito

--- t1/Ethernet.c
+++ t1/Ethernet.c
@@ -7,22 +7,17 @@
 
 CHAR_T*
 trace_ethernet(ETHERNET_HEADER* eth, int ID,  FRAME_HEADER *frh)
-{ 
+{
 	printf ("ETHER: ----- Ether Header -----\n");
 	printf ("ETHER:\n");
 	printf ("ETHER: Packet %d\n", ID);
-	printf ("ETHER: Packet size = %u bytes\n", (unsigned int) frh->capt_data);
-	
-		
-	
-	printf ("ETHER: Destination \t= %02X:%02X:%02X:%02X:%02X:%02X %s\n", eth->receiver[0], eth->receiver[1], eth->receiver[2], eth->receiver[3], eth->receiver[4], eth->receiver[5], (is_broadcast(eth->receiver)?"(brodcast)":""));
-	
+	printf ("ETHER: Packet size = %u bytes\n", (unsigned int) frh->capt_data);	
+	printf ("ETHER: Destination \t= %02X:%02X:%02X:%02X:%02X:%02X %s\n", eth->receiver[0], eth->receiver[1], eth->receiver[2], eth->receiver[3], eth->receiver[4], eth->receiver[5], (is_broadcast(eth->receiver)?"(brodcast)":""));	
 	printf ("ETHER: Source \t\t= %.02X:%02X:%02X:%02X:%02X:%.02X\n", eth->sender[0], eth->sender[1], eth->sender[2], eth->sender[3], eth->sender[4], eth->sender[5]);	
 	printf ("ETHER: Ethertype %04X %s\n",(unsigned int) ntohs(eth->type), ((unsigned int) ntohs(eth->type) == 0x800)?"(IP)":"(ARP)");
 	printf ("ETHER:\n");
 	return 0;
 }
-
 
 int 
 is_broadcast(BYTE * byte)
