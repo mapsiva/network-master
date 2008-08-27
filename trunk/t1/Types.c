6303b2dd34182e17909f34cc76d04c2c24e7778a
mapsiva <mapsiva@a04a7cc0-a954-0410-a178-6fe4ac297742>
2008-08-27 05:46:15 +0000

    mais uma versao do filtro, algom com keyword table

--- trunk/t1/Types.c
+++ trunk/t1/Types.c
@@ -1,9 +1,29 @@
 #ifndef TYPES_C_
 #define TYPE_C_
 #include "Types.h"
+#include "strings.h"
 
 WORD to_word(void * value)
 {
 	return (WORD) ((void*)value); 
 }
+
+tKeywordTableEntry*
+SearchKeyword(tKeywordTableEntry* k, const char * name)
+{
+    while (k->Name)
+    {
+		if (!strcasecmp((const char *)k->Name, (const char*)name))
+			return k;
+		k++;
+	}
+	return 0;
+}
+
+DECLARE_KEYWORD_TABLE();
+
+DEFINE_KEYWORD_TABLE()
+	KEYWORD( "int"   , 0, 0),
+END_KEYWORD_TABLE;
+
 #endif 
